5ce620fba9f1e1bff3470086ed0a22d3123ad6fe
Deen037 <deen.bds@gmail.com>
2023-12-05 22:10:13 +0100

    test

--- source/script.js
+++ source/script.js
@@ -36,7 +36,7 @@ const themes = {
     11: { name: "cover", img: "./source/lili/logo.jpg" },
     12: { name: "done", img: "./source/lili/done.jpg" },
   },
-  rebeka: { name: "Rebeka", img: "blabla" },
+  rebeka: { name: "Rebeka", img: "bla" },
 };
 
 let poles = [];
