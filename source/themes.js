0eeaae8f7f6fe98cf1e78152bf340a096dc23b1c
Deen037 <deen.bds@gmail.com>
2023-12-15 22:13:30 +0100

    fix

--- source/themes.js
+++ source/themes.js
@@ -23,10 +23,7 @@ const themes = {
     },
     4: { name: "Mateo", img: "./source/lili/mateo.jpeg" },
     5: { name: "Milan/Mateo/Lili", img: "./source/lili/milanMateoLili.jpeg" },
-    6: {
-      name: "Nikol/Stázi/Tom",
-      img: "./source/lili/nikolStaziTom.jpeg",
-    },
+    6: { name: "Nikol/Stázi/Tom", img: "./source/lili/nikolStaziTom.jpeg" },
     7: { name: "Petr/Líba", img: "./source/lili/petrLiba.jpeg" },
     8: { name: "Tomáš", img: "./source/lili/tomas.jpeg" },
     9: { name: "Stázinka", img: "./source/lili/stazinka.jpeg" },
@@ -38,16 +35,10 @@ const themes = {
   gfa: {
     1: { name: "Aleš", img: "./source/gfa/ales.png" },
     2: { name: "Dávid", img: "./source/gfa/david.png" },
-    3: {
-      name: "Ivana",
-      img: "./source/gfa/ivana.png",
-    },
+    3: { name: "Ivana", img: "./source/gfa/ivana.png" },
     4: { name: "Lukáš J.", img: "./source/gfa/lukasj.png" },
     5: { name: "Lukáš K.", img: "./source/gfa/lukask.png" },
-    6: {
-      name: "Matěj",
-      img: "./source/gfa/matej.png",
-    },
+    6: { name: "Matěj", img: "./source/gfa/matej.png" },
     7: { name: "mentor1", img: "./source/gfa/mentor1.png" },
     8: { name: "mentor2", img: "./source/gfa/mentor2.png" },
     9: { name: "Pavla", img: "./source/gfa/pavla.png" },
@@ -59,18 +50,12 @@ const themes = {
   rebenka: {
     1: { name: "babka", img: "./source/rebenka/babka.jpg" },
     2: { name: "dano", img: "./source/rebenka/dano.jpg" },
-    3: {
-      name: "dedko",
-      img: "./source/rebenka/dedko.jpg",
-    },
+    3: { name: "dedko", img: "./source/rebenka/dedko.jpg" },
     4: { name: "deko", img: "./source/rebenka/deko.jpg" },
     5: { name: "dusan", img: "./source/rebenka/dusan.jpg" },
-    6: {
-      name: "janko",
-      img: "./source/rebenka/janko.jpg",
-    },
+    6: { name: "janko", img: "./source/rebenka/janko.jpg" },
     7: { name: "manka", img: "./source/rebenka/manka.jpg" },
-    8: { name: "rebeka", img: "./source/rebenka/rebeka.jpg" },
+    8: { name: "rebeka", img: "./source/rebenka/rebeka.JPG" },
     9: { name: "tatik", img: "./source/rebenka/tatik.jpg" },
     10: { name: "baki", img: "./source/rebenka/baki.jpg" },
     11: { name: "cover", img: "./source/rebenka/cover.jpg" },
