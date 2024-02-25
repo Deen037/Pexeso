fff00586f5ca6322a6e4f650f1404157d7fd0f2f
Deen037 <deen.bds@gmail.com>
2024-02-25 09:14:25 +0100

    add stopwatch

--- source/script.js
+++ source/script.js
@@ -370,3 +370,45 @@ function enhanceNavbar() {
   document.getElementById(mode).style.color = "white";
 }
 enhanceNavbar();
+
+// Stopwatch
+
+let stopwatch = document.getElementById("time");
+
+let milisecodns = 0;
+let seconds = 0;
+let minutes = 0;
+let timer;
+
+function runTime() {
+  milisecodns++;
+
+  if (milisecodns === 10) {
+    milisecodns = 0;
+    seconds++;
+  }
+
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+  }
+}
+
+function pad(num) {
+  return num.toString().padStart(2, "0");
+}
+
+function start() {
+  timer = setInterval(() => {
+    runTime();
+    stopwatch.textContent =
+      pad(minutes) + ":" + pad(seconds) + ":" + milisecodns;
+  }, 100);
+}
+
+function stop() {
+  clearInterval(timer);
+  milisecodns = 0;
+  seconds = 0;
+  minutes = 0;
+}
